9da82fe19d724671c326b785aef1a161d04c29cd
Matej Drobnič <matejdro@gmail.com>
2017-09-09 08:13:47 +0200

    Haptic feedback when dragging around list items.

---
+++ src/main/java/com/matejdro/wearutils/miscutils/VibratorCompat.java
@@ -0,0 +1,18 @@
+package com.matejdro.wearutils.miscutils;
+
+import android.Manifest;
+import android.os.Build;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+import android.support.annotation.RequiresPermission;
+
+public class VibratorCompat {
+    @RequiresPermission(Manifest.permission.VIBRATE)
+    public static void vibrate(Vibrator vibrator, long timeMilliseconds) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            vibrator.vibrate(VibrationEffect.createOneShot(timeMilliseconds, -1));
+        } else {
+            vibrator.vibrate(timeMilliseconds);
+        }
+    }
+}

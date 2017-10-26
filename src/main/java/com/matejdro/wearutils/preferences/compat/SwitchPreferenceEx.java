8ec412e5fdd64b5012b76e6522afffc127eba018
Matej Drobnič <matejdro@gmail.com>
2017-10-26 15:18:06 +0200

    Started adding appcompat preferences.

---
+++ src/main/java/com/matejdro/wearutils/preferences/compat/SwitchPreferenceEx.java
@@ -0,0 +1,28 @@
+package com.matejdro.wearutils.preferences.compat;
+
+import android.content.Context;
+import android.support.v7.preference.PreferenceViewHolder;
+import android.support.v7.preference.SwitchPreferenceCompat;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * <p>SwitchPreference that will:</p>
+ * <p>
+ * <p>1. Obey HTML tags in summary</p>
+ * <p>2. Display title in two lines if it can't fit into one</p>
+ */
+public class SwitchPreferenceEx extends SwitchPreferenceCompat {
+    public SwitchPreferenceEx(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+
+    @Override
+    public void onBindViewHolder(PreferenceViewHolder holder) {
+        TextView titleView = (TextView) holder.findViewById(android.R.id.title);
+        titleView.setSingleLine(false);
+
+        super.onBindViewHolder(holder);
+    }
+}

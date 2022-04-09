036f6f272c49077e090a522f6e460b44e5c7ad67
Matej Drobnic <matejdro@gmail.com>
2022-04-09 08:35:06 +0200

    fix: return null byte array when asset is unavailable

--- src/main/java/com/matejdro/wearutils/messages/DataUtilsKt.kt
+++ src/main/java/com/matejdro/wearutils/messages/DataUtilsKt.kt
@@ -1,17 +1,27 @@
 package com.matejdro.wearutils.messages
 
+import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.wearable.DataClient
 import com.google.android.gms.wearable.DataItemAsset
+import com.google.android.gms.wearable.WearableStatusCodes
 import com.matejdro.wearutils.coroutines.await
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
-suspend fun DataClient.getByteArrayAsset(asset: DataItemAsset): ByteArray {
-    val inputStream = this.getFdForAsset(asset).await().inputStream
+suspend fun DataClient.getByteArrayAsset(asset: DataItemAsset): ByteArray? {
+    return try {
+        val inputStream = this.getFdForAsset(asset).await().inputStream
 
-    return withContext(Dispatchers.IO) {
-        inputStream.use {
-            it.readBytes()
+        withContext(Dispatchers.IO) {
+            inputStream.use {
+                it.readBytes()
+            }
+        }
+    } catch (e: ApiException) {
+        if (e.statusCode == WearableStatusCodes.ASSET_UNAVAILABLE) {
+            null
+        } else {
+            throw e
         }
     }
 }

30a35d1d0f4bbcce42b30dd41b34c658da2d0994
Matej Drobnic <matejdro@gmail.com>
2022-09-17 10:59:23 +0200

    chore: bump dependencies

--- src/main/java/com/matejdro/wearutils/preferences/compat/AutoSummaryListPreference.kt
+++ src/main/java/com/matejdro/wearutils/preferences/compat/AutoSummaryListPreference.kt
@@ -9,10 +9,10 @@ import androidx.preference.ListPreference
  * Variant of ListPreference that will automatically set its summary to the currently selected value
  */
 class AutoSummaryListPreference : ListPreference {
-    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int, defStyleRes: Int) : super(context, attrs, defStyleAttr, defStyleRes)
-    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
-    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
-    constructor(context: Context?) : super(context)
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int, defStyleRes: Int) : super(context, attrs, defStyleAttr, defStyleRes)
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context) : super(context)
 
     init {
         summary = getCurrentlySelectedValueDescription()
